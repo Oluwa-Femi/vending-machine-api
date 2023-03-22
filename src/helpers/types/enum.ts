c85877a04a45ce2a77edbf23c9dc40aca79b8704
oluwatolaio <oluwafemi.oluwatola@sterling.ng>
2023-03-22 13:03:19 +0100

    Updated markdown

--- src/helpers/types/enum.ts
+++ src/helpers/types/enum.ts
@@ -7,7 +7,7 @@ export enum Messages {
     STATUS400 = "",
     NOTFOUND = "Invalid Username or Password",
     LOGGEDOUT = "User is Logged Out",
-    DUPLICATEDSESSION = "You have an active session. Please logout your account",
+    DUPLICATEDSESSION = "There is already an active session using your account",
     STATUS500 = "An error occured. Please try again or contact support",
     SESSIONEXPIRED = "Unauthorized. Session timeout",
     INVALIDTOKEN = "An error occurred. Invalid token format",
